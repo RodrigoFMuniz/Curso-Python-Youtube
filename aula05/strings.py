ba94d999b31bb175366a23b2a577c0b0ed02ccd3
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-15 00:05:32 -0300

    Adicionadas demonstrações de métodos com strings

--- aula05/strings.py
+++ aula05/strings.py
@@ -1,9 +1,17 @@
-s1 = 'hello world !!!'
+s1 = '    hello world !!!    '
 s2 = 'Hello again ...'
 
 print(s1.capitalize())
 print(s1.title())
 print(s2.lower())
+print('Strip',s2.strip())
+print('rstrip',s2.rstrip())
+print('lstrip',s2.lstrip())
+print('ljust',s2.ljust(3))
+print('rstrip',s2.rjust(3))
+print('Replace',s2.replace('l','*'))
+print('Remove Suffix',s2.removesuffix('...'))
+print('Remove Prefix',s2.removeprefix('Hel'))
 print(s2.upper())
 print("Number of 'a'", '=', s2.count('a'))
 print("Number of 'l'", '=', s2.count('l'))
