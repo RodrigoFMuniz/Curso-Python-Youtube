005a00a2222e31ecad8ba3169695adbf1baf578f
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-18 00:57:20 -0300

    Aula 10: Sets, parte 3 - Pop

--- aula10/sets.py
+++ aula10/sets.py
@@ -8,3 +8,10 @@ print('Métodos ', dir(s1))
 
 print(s1.add(7))
 print('Set 1 após add: ',s1)
+
+#Pop
+
+print('Printando o retorno após o pop: ',s1.pop())
+print('Set 1 após pop: ',s1)
+print('Printando o retorno após o pop: ',s1.pop()) #Retira o primeiro item do set
+print('Set 1 após pop: ',s1)
