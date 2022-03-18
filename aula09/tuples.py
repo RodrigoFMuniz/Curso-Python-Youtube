6f53fd56623310a157a0e6dc9aabccabf0fbd8d4
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-17 23:34:15 -0300

    Aula 09: Tuplas, parte 02

--- aula09/tuples.py
+++ aula09/tuples.py
@@ -1,3 +1,6 @@
-t1 = (1,) # Com um elemento é necessária a inserção da virgula
-print(t1)
-print(type(t1))
+t1 = (1,1,1,10,3,3,5,6,3) # Com um elemento é necessária a inserção da virgula
+print('Tupla 1:',t1)
+print('Tipo da tupla:',type(t1))
+print(dir(t1))
+print('Contador de itens 1 : ',t1.count(1))
+print('Indice do item 10: ',t1.index(10))
