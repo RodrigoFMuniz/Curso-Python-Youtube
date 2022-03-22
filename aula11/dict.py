1b01dd7e3a0a7769c1d4a3b498c111abd56dec31
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-21 23:17:17 -0300

    Aula 11: Dicionários, parte 03, Update

--- aula11/dict.py
+++ aula11/dict.py
@@ -5,3 +5,10 @@ d1 = {'el_1': 'Valor 1'}
 print('Dict 1:',d1)
 print(dir(d1))
 
+# Update
+print('Dict 1 (update) antes:',d1)
+print('retorno de update: ', d1.update([{'el_2':'Valor 2','el_3': 'Valor 3'}]))
+print('Dict 1 (update) depois:',d1)
+
+
+
