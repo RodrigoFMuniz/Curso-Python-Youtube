b413c1605f86cfb470c1789a9950354da1bbb37e
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-21 23:21:43 -0300

    Aula 11: Dicionários, parte 04, Get

--- aula11/dict.py
+++ aula11/dict.py
@@ -7,8 +7,14 @@ print(dir(d1))
 
 # Update
 print('Dict 1 (update) antes:',d1)
-print('retorno de update: ', d1.update([{'el_2':'Valor 2','el_3': 'Valor 3'}]))
+print('retorno de update: ', d1.update({'el_2':'Valor 2','el_3': 'Valor 3'}))
 print('Dict 1 (update) depois:',d1)
+
+# Get
+
+print('Dict 1 (get) antes:',d1)
+print('retorno de Get: ', d1.get('el_2'))
+print('Dict 1 (get) Depois:',d1)
 
 
 
