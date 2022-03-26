9514bc53fb328cf9abae176d9c82d2a64b08e13e
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-25 21:44:41 -0300

    Aula 12: Ifs, parte 03: elif

--- aula12/ifs.py
+++ aula12/ifs.py
@@ -13,3 +13,18 @@ if b == 'Batman':
     print('É o batman')
 else:
     print(f'Não é o batman. É o {b}.')
+
+# Elif
+
+c = bin(12)
+
+if c =='0b1001':
+    print('Retornou 9 em binário')
+elif c == '0b1100':
+    print('Retornou 12 em binário')
+elif c == '0b1000':
+    print('Retornou 8 em binário')
+elif c == '0b1010':
+    print('Retornou 10 em binário')
+else:
+    print(f'Outro número {c}')
