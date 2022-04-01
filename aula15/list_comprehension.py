1ca2168b3c83c5846586c7e96bb34e88fcfc21ee
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-03-31 23:11:52 -0300

    Aula 15: List comprehension, parte 03

--- aula15/list_comprehension.py
+++ aula15/list_comprehension.py
@@ -1,4 +1,7 @@
 l1 = [x for x in range(10)]
 l2 = list(range(0, 10))
+l3 = ['Banana', 'Maçã', 'Laranja', 'Jamelão', 'Cacau', 'Kiwi', 'Manga']
+
 print(l1)
 print(l2)
+print([x for x in l3 if 'Ca' in x])
