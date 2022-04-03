a9ed4b809a698b37b2afe03726b1f190574559e0
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-02 23:26:17 -0300

    Aula 18: destructuring, parte 03, dic

--- aula18/destructuring.py
+++ aula18/destructuring.py
@@ -9,3 +9,9 @@ d1, d2, *d3 = dic1
 print(d1)
 print(d2)
 print(d3)
+
+dic2 = {'var 1':'value 1', 'var 2':'value 2','var 3':'value 3','var 4':'value 4'}
+d4, d5, *d6 = dic2.items()
+print(d4)
+print(d5)
+print(d6)
