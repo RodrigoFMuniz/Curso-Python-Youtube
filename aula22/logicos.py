2bf1dc50285aa16fd0190043d06e208124b8d2db
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-03 23:19:33 -0300

    Aula 21: Lógicos

---
+++ aula22/logicos.py
@@ -0,0 +1,18 @@
+a = 20
+b = 30
+c = None
+d = False
+
+print("a == b:",a == b)
+print("b == c:",b == c)
+print("a != b:",a != b)
+print("b != c:",b != c)
+print("not b:",not b)
+print("not c:",not c)
+print("not d:",not d)
+print("a and b:",a and b)
+print("b and c:",b and c)
+print("b and d:",b and d)
+print("a or b:",a or b)
+print("b or c:",b or c)
+print("d or b:",d or b)
