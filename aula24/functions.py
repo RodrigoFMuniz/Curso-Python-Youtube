92339629a161f7f6f05b2783d7e2be2cbb1e1f47
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-05 23:53:41 -0300

    Aula 24: functions

--- aula24/functions.py
+++ aula24/functions.py
@@ -1,11 +1,11 @@
 # functions
-total = 0
-def sum(*values):
-    for n in values:
-        total += n
+
+def sum(*v):
+    total = 0
+    for n in v[0]:
+        total +=  n
     return total
 
 values = [1,2,3,4,5,6,7,8,9,10]
 
-# print(sum(values))
-print(total)
+print(sum(values))
