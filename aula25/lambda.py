454e1ef39bfead1974324141ecd259695de28caf
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-09 23:56:41 -0300

    Aula 25. lambda, parte 03 com reduce

--- aula25/lambda.py
+++ aula25/lambda.py
@@ -1,11 +1,17 @@
+from functools import reduce
 # lambda
+l1 = [1,2,3,4,5]
+
 a = lambda x: x**2
 print(a(6))
 
 #lambda em map
-l1 = [1,2,3,4,5]
-
 b = map(lambda x: x**2, l1)
 
 for it in b:
     print(it)
+
+
+#lambda em reduce
+c = reduce(lambda total, x : total + x, l1, 0)
+print('Total do reduce: ', c)
