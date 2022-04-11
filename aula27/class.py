d57c394de6923de12701eff97dc0eaf9c43cf15c
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-10 23:47:07 -0300

    Aula 27, class, parte 03

--- aula27/class.py
+++ aula27/class.py
@@ -1,25 +1,29 @@
 class Pessoa:
     def __init__(self):
-        self.nome = None
+        self._nome = None
     
     def __repr__(self):
-        return self.nome
+        return self._nome
 
     def __str__(self):
-        return self.nome
+        return self._nome
+    
+    def get_nome(self):
+        return self._nome
 
 
 
 pessoa_1 = Pessoa()
 
-print(pessoa_1.nome)
-print(type(pessoa_1.nome))
+print(pessoa_1._nome)
+print(type(pessoa_1._nome))
 print(pessoa_1.__repr__())
 print(pessoa_1.__str__())
 
-pessoa_1.nome='Rodrigo'
+pessoa_1._nome='Rodrigo'
 
-print(pessoa_1.nome)
-print(type(pessoa_1.nome))
+print(pessoa_1._nome)
+print(type(pessoa_1._nome))
 print(pessoa_1.__repr__())
 print(pessoa_1.__str__())
+print('Get:',pessoa_1.get_nome())
