f9039cf96c6292c79e5c8f6c61a0f326a513c4b1
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-16 23:18:24 -0300

    Aula 27, @setter

--- aula27/setter_and_getter.py
+++ aula27/setter_and_getter.py
@@ -11,10 +11,26 @@ class Person:
     @property
     def surname(self):
         return self.__surname
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @surname.setter
+    def surname(self, surname):
+        self.__surname = surname
 
 p1 = Person('Rodrigo','Muniz')
 p2 = Person('Fernando','Muniz')
 
+print(p1.name)
+print(p2.name)
+print(p1.surname)
+print(p2.surname)
+p1.name = "RODRIGO"
+p1.surname = "Fernandes Muniz"
+p2.name = "FERNANDO"
+p2.surname = "Fernandes Muniz"
 print(p1.name)
 print(p2.name)
 print(p1.surname)
