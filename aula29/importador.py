283e7ceb3a51b0c28ca371b732a7b888e20916fa
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-21 23:36:32 -0300

    Aula 29: função main, parte 02

---
+++ aula29/importador.py
@@ -0,0 +1,5 @@
+from main import sum
+
+print(sum([1,2,3,4]))
+
+print(f'Executando {sum.__name__} importado por este módulo {__name__} ')
--- aula29/main.py
+++ aula29/main.py
@@ -2,6 +2,7 @@ def sum(lista = []):
     total = 0
     for i in lista:
         total+= int(i)
+    return total
 
 if __name__ == "__main__":
-    print(f'Chamou a função {sum.__name__}')
+    print(f'Chamou a função {sum.__name__} a partir do módulo main.py, quando chamado em {__name__}')
