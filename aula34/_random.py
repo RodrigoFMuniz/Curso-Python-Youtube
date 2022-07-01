35e7c35f0c741390b0a5b0cdb711d1dc84b41cc3
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-01 16:42:46 -0300

    Added aula 34, choices

--- aula34/_random.py
+++ aula34/_random.py
@@ -16,3 +16,8 @@ print(rd3)
 x = [1, 2, 3, 1, 'e', 'tr', 'er-3', 34]
 rd4 = random.choice(x)
 print(rd4)
+
+# Choices
+x = [1, 2, 3, 1, 'e', 'tr', 'er-3', 34]
+rd5 = random.choices(x, [1.0, 0.3, 0.2, 0.3, 0.2, 0.3, 0.2, 0.3])
+print(rd5)
