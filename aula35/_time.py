ab846e9e186245a0cf3aece806b4bb8c2fbfc586
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-02 23:46:45 -0300

    Added aula 35, intervalo

--- aula35/_time.py
+++ aula35/_time.py
@@ -7,3 +7,14 @@ print(t1)
 # gmtime
 t2 = tm.gmtime(t1)
 print(t2)
+
+# Interval
+antes = tm.time()
+x = 0
+for i in range(0, 10000):
+    x += 1
+depois = tm.time()
+
+intervalo = depois - antes
+
+print(f'{intervalo*1000} ms')
