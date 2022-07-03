f63fe4e8edd186ec7b90d9c18ba2837e082e6c5a
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-02 23:57:11 -0300

    Added aula 36, Tipos de erros

---
+++ aula36/_erros.py
@@ -0,0 +1,8 @@
+'''
+NameError: A variável não foi definida
+TypeError: Tipos de dados incompatíveis
+RuntimeError: Erro de execução
+SyntaxError: A sintaxe digitada é inválida e não reconhecida pelo compilador
+ZeroDivisionError: Divisão por zero
+IndexError: Índice está fora da correção
+'''
