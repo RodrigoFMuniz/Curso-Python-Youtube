8fbc709b93da9b17b1cac312f020a6093a88331f
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-07-04 23:15:40 -0300

    Aula 36, erros, ZeroDivisionError

--- aula36/_erros.py
+++ aula36/_erros.py
@@ -10,9 +10,13 @@ ValueError: Erro de valor inserido
 
 # NameError
 
+from decimal import DivisionByZero
+
+
 try:
-    name = {'oi':'olá'}
-    print(name['oi'])
+    # name = {'oi':'olá'}
+    x = 1/0
+    print(x)
 except NameError as error:
     print(f'error do tipo NameError: {error}')
 except ValueError as error:
@@ -21,5 +25,7 @@ except KeyError as error:
     print(f'Erro do tipo KeyError: {error}')
 except IndexError as error:
     print(f'Erro do tipo IndexError: {error}')
+except ZeroDivisionError as error:
+    print(f'Erro do tipo ZeroDivisionError: {error}')
 else:
     print('Finalizado')
