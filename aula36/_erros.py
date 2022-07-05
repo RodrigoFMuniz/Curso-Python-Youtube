4b094feaa9339fce4eccae1cb58a8506cdb8376e
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-07-04 22:49:10 -0300

    Aula 36, erros, ValueError

--- aula36/_erros.py
+++ aula36/_erros.py
@@ -5,13 +5,17 @@ RuntimeError: Erro de execução
 SyntaxError: A sintaxe digitada é inválida e não reconhecida pelo compilador
 ZeroDivisionError: Divisão por zero
 IndexError: Índice está fora da correção
+ValueError: Erro de valor inserido
 '''
 
 # NameError
 
 try:
-    print(name)
+    name = 'Rodrigo'
+    print(int(name))
 except NameError as error:
     print(f'error: {error}')
+except ValueError as error:
+    print(f'Erro: {error}')
 else:
     print('Finalizado')
