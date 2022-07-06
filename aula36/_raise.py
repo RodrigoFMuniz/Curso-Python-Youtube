55adbe69e4e1ed23f90aba8e9ead2c11ba21ebc2
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-07-05 22:04:48 -0300

    Aula 36, raise, TypeError

--- aula36/_raise.py
+++ aula36/_raise.py
@@ -2,6 +2,10 @@ def sum(n1,n2):
     try:
         return n1+n2
     except Exception as e:
-        print('Erro: ',e)
+        # print('Erro: ',e) #modifica o comportamento da linguagem python
+        raise 
 
-sum(1,'w')
+try:
+    sum(1,'w')
+except TypeError as e:
+    print('Depois do raise: ',e)
