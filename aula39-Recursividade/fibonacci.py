a514a9a0e66f92d28765bf29601751819bd70ad2
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-11 21:59:40 -0300

    Aula 39 - Recursividade sem memo

--- aula39-Recursividade/fibonacci.py
+++ aula39-Recursividade/fibonacci.py
@@ -3,11 +3,17 @@
 
 fib1() retorna falha de recursão
 RecursionError: maximum recursion depth exceeded
+
+fib2(): Apresenta falha para valores de n muito grandes, por conta da PG de operações recursivas
 '''
-def fib1(n:int)->int:
-    return fib1(n-1)-fib1(n-2)
-    
+# def fib1(n:int)->int:
+#     return fib1(n-1)+fib1(n-2)
+
+def fib2(n:int)->int:
+    if n<2:
+        return n
+    return fib2(n-1)+ fib2(n-2)
 
 if __name__== "__main__":
-    fib1(2)
+    print(fib2(50))
     print(__doc__)
