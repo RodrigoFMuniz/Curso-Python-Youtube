376087ab08cd6b9f9f6183e20b634cbbf23e7a67
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-11 22:15:35 -0300

    Aula 39 - Recursividade lru_cache annotation

--- aula39-Recursividade/fibonacci.py
+++ aula39-Recursividade/fibonacci.py
@@ -8,22 +8,30 @@ fib2(): Apresenta falha para valores de n muito grandes, por conta da PG de oper
 fib3(): Utiliza um objeto do tipo memo para ganhar desempenho
 '''
 from typing import Dict
+from functools import lru_cache
+
 # def fib1(n:int)->int:
 #     return fib1(n-1)+fib1(n-2)
 
 # def fib2(n:int)->int:
 #     if n<2:
 #         return n
-#     return fib2(n-1)+ fib2(n-2)
+#     return fib2(n-2)+ fib2(n-1)
 
 
-memo: Dict[int, int] = {0:0, 1:1}
+# memo: Dict[int, int] = {0:0, 1:1}
 
-def fib3(n:int)->int:
-    if n not in memo:
-        memo[n] = fib3(n-1) + fib3(n-1)
-    return memo[n]
+# def fib3(n:int)->int:
+#     if n not in memo:
+#         memo[n] = fib3(n-2) + fib3(n-1)
+#     return memo[n]
+
+@lru_cache(maxsize=None)
+def fib4(n:int)->int:
+    if n<2:
+        return n
+    return fib4(n-2)+ fib4(n-1)
 
 if __name__== "__main__":
-    print(fib3(10))
+    print(fib4(9))
     # print(__doc__)
